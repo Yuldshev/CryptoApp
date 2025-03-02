9eec6d33c79d8050a8523ddb0e523acb8ff8dd5f
Yuldashev <ikrom921@gmail.com>
2025-03-02 18:32:06 +0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Yuldashev Fazliddin on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
